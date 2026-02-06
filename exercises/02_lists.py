2629f0d7cbf280ad75c9a00f23408a974c49e4e3
student <rkutner4@student.touro.edu>
2026-02-05 19:24:19 -0500

    Finished Assignment- compeleted TODOs

--- exercises/02_lists.py
+++ exercises/02_lists.py
@@ -8,3 +8,17 @@ TODO:
 6. List comprehension for the lengths of each food item -
  create a new list where each item is  the length of the corresponding food item in the original list.
 """
+favorite_foods = ["salad", "steak", "icecream"]
+
+print(favorite_foods[0])
+print(favorite_foods[2])
+
+favorite_foods.append("sushi")
+
+favorite_foods.remove("sushi")
+
+for i in favorite_foods:
+    print(i)
+
+food_lengths = [len(i) for i in favorite_foods]
+print(food_lengths)
--- exercises/04_dictionaries.py
+++ exercises/04_dictionaries.py
@@ -2,3 +2,12 @@
 TODO:
 Create dictionary for a book
 """
+book = {
+    "title": "My Journal",
+    "author": "Rena Kutner",
+    "year_published": 2015,
+    "genre": "Realistic Fiction",
+    "pages": 342
+}
+
+print(book)
